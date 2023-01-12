40dd5ea6e347d62135e6841c2735b69de8d49dc8
Julio Quezada <julio.quezada@catolica.edu.sv>
2023-01-12 12:50:51 -0600

    Testing deployment for GitHub Pages

--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -1,13 +1,13 @@
 import React from 'react';
 
-const SearchBox = ({searchfield, searchchChange}) => {
+const SearchBox = ({searchfield, searchChange}) => {
     return (
         <div className='pa2'>
          <input
            className='pa3 ba b--green bg-lightest-blue' 
            type='search' 
            placeholder='search robots' 
-           onChange={searchchChange}
+           onChange={searchChange}
         />
         </div>
 
