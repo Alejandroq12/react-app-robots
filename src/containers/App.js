7c261ef05362ab99acd6dfb266fb6e73c880ca54
Julio Quezada <julio.quezada@catolica.edu.sv>
2023-07-02 17:25:31 -0600

    App ready with React.js Hooks

--- src/containers/App.js
+++ src/containers/App.js
@@ -5,21 +5,18 @@ import Scroll from '../components/Scroll';
 import './App.css';
 
 function App() {
-  // constructor() {
-  //   super()
-  //   this.state = {
-  //     robots: [],
-  //     searchfield: ''
-  //   }
-  // }
-
-  // componentDidMount() {
-  //   fetch('https://jsonplaceholder.typicode.com/users')
-  //     .then(response=> response.json())
-  //     .then(users => {this.setState({ robots: users})});
-  // }
   const [robots, setRobots] = useState([]);
   const [searchfield, setSearchfield] = useState('');
+  // const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then((response) => response.json())
+      .then((users) => {
+        setRobots(users);
+      });
+  //    console.log(count);
+  }, []); //[count]);
 
   const onSearchChange = (event) => {
     setSearchfield(event.target.value);
@@ -28,11 +25,14 @@ function App() {
   const filteredRobots = robots.filter((robot) => {
     return robot.name.toLowerCase().includes(searchfield.toLowerCase());
   });
+
+  // console.log(robots, searchfield);
   return !robots.length ? (
     <h1>Loading</h1>
   ) : (
     <div className="tc">
       <h1 className="f1">RoboFriends</h1>
+      {/* <button onClick={() => setCount(count+1)}></button> */}
       <SearchBox searchChange={onSearchChange} />
       <Scroll>
         <CardList robots={filteredRobots} />
